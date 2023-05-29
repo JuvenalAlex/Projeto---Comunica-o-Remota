ad34df7155c985d7cb3f53ca67c94a90bd415a81
JuvenalAlex <61656798+JuvenalAlex@users.noreply.github.com>
2023-05-29 15:50:48 -0300

    Update server.py

--- server.py
+++ server.py
@@ -3,12 +3,21 @@ import Pyro4
 @Pyro4.expose
 class MeuServicoRemoto(object):
     def saudacao(self):
-        return "Olá, mundo!"
+        return "Servidor Conectado!"
 
-    def somar(self, a, b):
-        return a + b
+    def calcular_bhaskara(self, a, b, c):
+        delta = b**2 - 4*a*c
+        if delta < 0:
+            return "A equação não possui raízes reais."
+        elif delta == 0:
+            raiz = -b / (2*a)
+            return "A equação possui uma raiz real: x = {:.2f}".format(raiz)
+        else:
+            raiz1 = (-b + delta**0.5) / (2*a)
+            raiz2 = (-b - delta**0.5) / (2*a)
+            return "A equação possui duas raízes reais: x' = {:.2f} e x'' = {:.2f}".format(raiz1, raiz2)
 
-daemon = Pyro4.Daemon(host = '192.168.1.114', port= 9090)
+daemon = Pyro4.Daemon(host='127.0.0.1', port=9090)
 uri = daemon.register(MeuServicoRemoto)
 
 print("URI do objeto remoto:", uri)
